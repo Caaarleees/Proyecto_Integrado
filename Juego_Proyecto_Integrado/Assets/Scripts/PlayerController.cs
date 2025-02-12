e4b65f5f147d0ed9a45683894f35f7f74140d4b2
luis-villalba-ponce <luvipo06@gmail.com>
2025-02-12 12:15:25 +0100

    control

--- Juego_Proyecto_Integrado/Assets/Scripts/PlayerController.cs
+++ Juego_Proyecto_Integrado/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Variables de referencia 
+    private Rigidbody2D playerRb;
+    private Animator anim;
+    private float horizontalInput;
 
     //Varibales de estadísticas del player
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.velocity = new Vector2(horizontalInput * speed, playerRb.velocity.y);
+    }
+
 }
