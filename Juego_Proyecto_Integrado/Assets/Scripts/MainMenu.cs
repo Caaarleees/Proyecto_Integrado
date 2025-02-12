f7463ba83ed72e9a4dfc264188f727d1db58f51a
Caaarleees <martinezortizcarles8@gmail.com>
2025-02-13 00:52:23 +0100

    13 subida

---
+++ Juego_Proyecto_Integrado/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        GameManager.instance.ChangeState(GameManager.GameState.GameRunning);
+    }
+
+    void Start()
+    {
+        UpdateScoreUI();
+    }
+
+
+
+        
+
+
+
+
+
+}
+
